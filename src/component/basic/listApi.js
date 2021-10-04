7edd7e21dfc4b7e7b35356661579ac9a38f763da
Devansh-gaur-1611 <devansh.gaur.iitbhu20@gmail.com>
2021-10-05 00:57:27 +0530

    Added my name-card to listApi.js

--- src/component/basic/listApi.js
+++ src/component/basic/listApi.js
@@ -1,35 +1,38 @@
 const List = [
-{
-image: "/images/male.png",
-firstName: "Akash",
-lastName: "Das",
-link: "https://github.com/astroidworld",
-},
+  {
+    image: "/images/male.png",
+    firstName: "Akash",
+    lastName: "Das",
+    link: "https://github.com/astroidworld",
+  },
 
-{
-image: "/images/male.png",
-firstName: "Soudip",
-lastName: "Das",
-link: "https://github.com/front-runner-sd",
-},    
-    
-{
-image:"/images/male.png",
-firstName: "Dhanushka",
-lastName: "Ranasinghe",
-link: "https://github.com/DhanushkaRanasinghe",
-}, 
-  
-{
-image: "/images/female.png",
-firstName: "Olga",
-lastName: "Tkach",
-link: "https://github.com/tkacholga",
-},
-  
-  
-  
-  
+  {
+    image: "/images/male.png",
+    firstName: "Soudip",
+    lastName: "Das",
+    link: "https://github.com/front-runner-sd",
+  },
+
+  {
+    image: "/images/male.png",
+    firstName: "Dhanushka",
+    lastName: "Ranasinghe",
+    link: "https://github.com/DhanushkaRanasinghe",
+  },
+
+  {
+    image: "/images/female.png",
+    firstName: "Olga",
+    lastName: "Tkach",
+    link: "https://github.com/tkacholga",
+  },
+
+  {
+    image: "/images/male.png",
+    firstName: "Devansh",
+    lastName: "Gaur",
+    link: "https://github.com/Devansh-gaur-1611",
+  },
 ];
 
 export default List;
