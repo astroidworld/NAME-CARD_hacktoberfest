35c5606554e496bdbdc598b200a13449971e3486
Astroidworld <astroidworld@gmail.com>
2021-10-04 14:43:40 +0530

    online hosting2

--- src/component/basic/listApi.js
+++ src/component/basic/listApi.js
@@ -1,7 +1,14 @@
 const List = [
     {
-        image:"/images/male.png",
-        firstName: "Akash",
+    image:"/images/male.png",
+    firstName: "Akash",
+    lastName: "Das",
+    link: "https://github.com/astroidworld",
+    }, 
+
+    {
+        image:"/images/female.png",
+        firstName: "deep(lala)",
         lastName: "Das",
         link: "https://github.com/astroidworld",
         }, 
