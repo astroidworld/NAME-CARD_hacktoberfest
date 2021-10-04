e3a3c04ea33ff177a8567c841ef1c47e54a67d47
Olga Tkach <tkacholya@gmail.com>
2021-10-04 15:00:18 -0400

    Added a new name card

--- src/component/basic/listApi.js
+++ src/component/basic/listApi.js
@@ -1,25 +1,24 @@
 const List = [
-    {
-    image:"/images/male.png",
+  {
+    image: "/images/male.png",
     firstName: "Akash",
     lastName: "Das",
     link: "https://github.com/astroidworld",
-    }, 
+  },
 
-    {
-    image:"/images/male.png",
+  {
+    image: "/images/male.png",
     firstName: "Soudip",
     lastName: "Das",
     link: "https://github.com/front-runner-sd",
-    }, 
-        
-        
-    
-    
-    
+  },
 
-        
-        
+  {
+    image: "/images/female.png",
+    firstName: "Olga",
+    lastName: "Tkach",
+    link: "https://github.com/tkacholga",
+  },
 ];
 
 export default List;
