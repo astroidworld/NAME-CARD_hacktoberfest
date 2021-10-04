e78619a4480756f608c8eb5fb64aa58f11419f83
Baibhav <baibhav030501@gmail.com>
2021-10-05 01:10:04 +0530

    baibhav0305

--- src/component/basic/listApi.js
+++ src/component/basic/listApi.js
@@ -1,35 +1,38 @@
 const List = [
-{
-image: "/images/male.png",
-firstName: "Akash",
-lastName: "Das",
-link: "https://github.com/astroidworld",
-},
+  {
+    image: "/images/male.png",
+    firstName: "Akash",
+    lastName: "Das",
+    link: "https://github.com/astroidworld",
+  },
 
-{
-image: "/images/male.png",
-firstName: "Soudip",
-lastName: "Das",
-link: "https://github.com/front-runner-sd",
-},    
-    
-{
-image:"/images/male.png",
-firstName: "Dhanushka",
-lastName: "Ranasinghe",
-link: "https://github.com/DhanushkaRanasinghe",
-}, 
-  
-{
-image: "/images/female.png",
-firstName: "Olga",
-lastName: "Tkach",
-link: "https://github.com/tkacholga",
-},
-  
-  
-  
-  
+  {
+    image: "/images/male.png",
+    firstName: "Soudip",
+    lastName: "Das",
+    link: "https://github.com/front-runner-sd",
+  },
+
+  {
+    image: "/images/male.png",
+    firstName: "Dhanushka",
+    lastName: "Ranasinghe",
+    link: "https://github.com/DhanushkaRanasinghe",
+  },
+
+  {
+    image: "/images/female.png",
+    firstName: "Olga",
+    lastName: "Tkach",
+    link: "https://github.com/tkacholga",
+  },
+
+  {
+    image: "/images/male.png",
+    firstName: "Baibhav",
+    lastName: "Panda",
+    link: "https://github.com/baibhav0305",
+  },
 ];
 
 export default List;
