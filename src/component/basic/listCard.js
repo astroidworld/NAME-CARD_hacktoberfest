c400de9e92ee1f94022570951268134b25528ae5
Astroidworld <astroidworld@gmail.com>
2021-10-06 00:24:00 +0530

    try 2 fix12

--- src/component/basic/listCard.js
+++ src/component/basic/listCard.js
@@ -12,8 +12,8 @@ const ListCard = ({listdata}) => {
                             <div class="content">
                                 {curElement.image.charAt(0) != "/"
                                     ? <img src={curElement.image} class="avatar-github" alt="avatar"></img>
-                                    : <img src={curElement.image} alt="avatar"></img>}
-                                {/* {<img src={curElement.image} alt="avatar"></img>} */}
+                                    : <img src={curElement.image} class="avatar" alt="avatar"></img>}
+                                 {/*<img src={curElement.image} alt="avatar"></img>*/}
                                 <h3>{curElement.firstName} {curElement.lastName}</h3>
                                 <h4>Now I'm a open source contributer.</h4>
                                 <a href={curElement.link} target="_blank" rel="noreferrer">Visit</a>
