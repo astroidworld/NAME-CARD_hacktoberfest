a44279eb7f2e977cc656fbe89d7631bb2e217cad
Aniket Nayek <nayekaniket2017@gmail.com>
2021-10-06 15:11:00 +0530

    Added Myself

--- src/component/basic/listApi.js
+++ src/component/basic/listApi.js
@@ -193,6 +193,14 @@ const List = [
 
     {
         image: "./male.png",
+        firstName: "Aniket",
+        lastName: "Nayek",
+        link: "https://github.com/AniketNayek3101",
+
+    },
+
+    {
+        image: "./male.png",
         firstName: "Arsh",
         lastName: "Manva",
         link: "https://github.com/hidekiriyuga",
