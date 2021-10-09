4c987ba931dd8b6b5cb1692af33df3cbd05f011a
Rishav Jaiswal <rishavjaiss14@gmail.com>
2021-10-09 21:09:01 +0530

    Update listApi.js

--- src/component/basic/listApi.js
+++ src/component/basic/listApi.js
@@ -286,6 +286,12 @@ const List = [
         lastName: "Lenka",
         link: "https://github.com/AnkitLenka26/",
     },
+	 {
+        image:"./male.png",
+        firstName: "Rishav",
+        lastName: "Jaiswal",
+        link: "https://github.com/rishavjaiss/",
+    },
 
 ];
 
